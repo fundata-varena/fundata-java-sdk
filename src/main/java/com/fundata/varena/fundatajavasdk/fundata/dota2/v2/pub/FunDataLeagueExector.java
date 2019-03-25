7ece72519f11d7079fcda2a55eb7e2b824e09a60
Razor0719 <631960509@qq.com>
2019-03-25 16:26:23 +0800

    public api(league, match)

---
+++ src/main/java/com/fundata/varena/fundatajavasdk/fundata/dota2/v2/pub/FunDataLeagueExector.java
@@ -0,0 +1,44 @@
+package com.fundata.varena.fundatajavasdk.fundata.dota2.v2.pub;
+
+import com.fundata.varena.fundatajavasdk.domain.entity.FunDataResult;
+import com.fundata.varena.fundatajavasdk.domain.exception.ClientException;
+import com.fundata.varena.fundatajavasdk.domain.http.Form;
+import com.fundata.varena.fundatajavasdk.fundata.FunDataExector;
+
+/**
+ * @author baoyl
+ * @created 2019/3/25
+ */
+public class FunDataLeagueExector extends FunDataExector {
+
+    public FunDataLeagueExector(String key, String secret) {
+        super(key, secret, "/fundata-dota2-free/v2/league");
+    }
+
+    private Form makePageByPageSize(int pageSize, int page) {
+        return new Form().add("page_size", pageSize)
+                .add("page", page);
+    }
+
+    private Form makePageByLimit(int page, int limit) {
+        return new Form().add("page", page).add("limit", limit);
+    }
+
+    public FunDataResult getLeagueList(int pageSize, int page) throws ClientException {
+        return clientOperation.get("/list", makePageByPageSize(pageSize, page));
+    }
+
+    public FunDataResult getLeagueDetail(int leagueId) throws ClientException {
+        Form form = new Form().add("league_id", leagueId);
+        return clientOperation.get("/detail", form);
+    }
+
+    public FunDataResult getLeaguePlayer(int playerId) throws ClientException {
+        Form form = new Form().add("player_id", playerId);
+        return clientOperation.get("/player", form);
+    }
+
+    public FunDataResult getLeagueTeam(int page, int limit) throws ClientException {
+        return clientOperation.get("/team", makePageByLimit(page, limit));
+    }
+}
---
+++ src/main/java/com/fundata/varena/fundatajavasdk/fundata/dota2/v2/pub/FunDataMatchExector.java
@@ -0,0 +1,70 @@
+package com.fundata.varena.fundatajavasdk.fundata.dota2.v2.pub;
+
+import java.util.Date;
+
+import com.fundata.varena.fundatajavasdk.domain.entity.FunDataResult;
+import com.fundata.varena.fundatajavasdk.domain.exception.ClientException;
+import com.fundata.varena.fundatajavasdk.domain.http.Form;
+import com.fundata.varena.fundatajavasdk.fundata.FunDataExector;
+
+/**
+ * @author baoyl
+ * @created 2019/3/25
+ */
+public class FunDataMatchExector extends FunDataExector {
+
+    public FunDataMatchExector(String key, String secret) {
+        super(key, secret, "/fundata-dota2-free/v2/match");
+    }
+
+    private Form makeMatchForm(int matchId) {
+        return new Form().add("match_id", matchId);
+    }
+
+    public FunDataResult getMatchBasicInfo(int matchId) throws ClientException {
+        return clientOperation.get("/basic-info", makeMatchForm(matchId));
+    }
+
+    public FunDataResult getMatchBanPicks(int matchId) throws ClientException {
+        return clientOperation.get("/ban-picks", makeMatchForm(matchId));
+    }
+
+    public FunDataResult getMatchGeneralInfo(int matchId) throws ClientException {
+        return clientOperation.get("/general-info", makeMatchForm(matchId));
+    }
+
+    public FunDataResult getMatchPlayers(int matchId) throws ClientException {
+        return clientOperation.get("/players", makeMatchForm(matchId));
+    }
+
+    public FunDataResult getMatchPlayersAbilityUpgrades(int matchId) throws ClientException {
+        return clientOperation.get("/players-ability-upgrades", makeMatchForm(matchId));
+    }
+
+    private Form makeMatchBatchForm(Date startTime, Date startFrom, int limit) {
+        limit = limit < 0 ? 0 : limit;
+        limit = limit > 200 ? 200 : limit;
+        Form form = new Form().add("start_time", startTime.getTime() / 1000L)
+                .add("limit", limit);
+        if (startFrom != null) {
+            form.add("start_from", startFrom.getTime() / 1000L);
+        }
+        return form;
+    }
+
+    public FunDataResult batchMatchBasicInfo(Date startTime, Date startFrom, int limit) throws ClientException {
+        return clientOperation.get("/basic-info/batch", makeMatchBatchForm(startTime, startFrom, limit));
+    }
+
+    public FunDataResult batchMatchBanPicks(Date startTime, Date startFrom, int limit) throws ClientException {
+        return clientOperation.get("/ban-picks/batch", makeMatchBatchForm(startTime, startFrom, limit));
+    }
+
+    public FunDataResult batchMatchPlayers(Date startTime, Date startFrom, int limit) throws ClientException {
+        return clientOperation.get("/players/batch", makeMatchBatchForm(startTime, startFrom, limit));
+    }
+
+    public FunDataResult batchMatchPlayersAbilityUpgrades(Date startTime, Date startFrom, int limit) throws ClientException {
+        return clientOperation.get("/players-ability-upgrades/batch", makeMatchBatchForm(startTime, startFrom, limit));
+    }
+}
